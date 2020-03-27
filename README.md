8c1a8fd1f67a060d2f11f353e3244755d4ec40e3
Serhii Pytliak <pytliak.serhii@gmail.com>
2020-03-27 11:20:09 +0200

    The files have been added. Starting to write the sysinfo script

---
+++ README.md
@@ -0,0 +1,1 @@
+GL DevOps ProCamp
---
+++ sysinfo.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# ---------------------------------------------------
+# Program by Pytliak Serhii <pytliak.serhii@gmail.com>
+# Version  Date       Info
+# 1.0      26.03.20   System info - metrics
+# ----------------------------------------------------
+# -*- coding: utf-8 -*-
+import psutil, sys, os, time, datetime
