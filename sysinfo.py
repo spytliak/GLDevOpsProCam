e0835cd2a932a9bcd071f15141695f1da0cc2585
Serhii Pytliak <pytliak.serhii@gmail.com>
2020-03-27 12:25:31 +0200

    Added memory

--- sysinfo.py
+++ sysinfo.py
@@ -12,15 +12,14 @@ import psutil, sys, os, time, datetime
 OS = os.uname()
 print('HOSTNAME:', OS[1], 'RELEASE:', OS[2], 'VERSION:', OS[4])
 
+
 #Uptime
 MINUTES = 60
 HOUR = MINUTES * 60
 DAY = HOUR * 24
-
 bootTIME = psutil.boot_time()
 NOW = time.time()
 s = NOW - bootTIME
-
 d = h = m = 0
 d = s / DAY
 s = s % DAY
@@ -28,14 +27,13 @@ h = s / HOUR
 s = s % HOUR
 m = s / MINUTES
 s = s % MINUTES
-
 uptime = ""
 if d > 1:
     uptime = "%d days, "%d
 elif d == 1:
     uptime = "1 day, "
-
 print("Uptime: " + uptime + "%d hours, %02d minutes, %02d seconds!"%(h,m,s))
+
 
 #Localtime
 localtime = time.asctime( time.localtime(time.time()) )
@@ -45,3 +43,19 @@ print ('Local current time:', localtime)
 #users
 for user in psutil.users():
     print('Authorized Users:', user.name, 'TTY', user.terminal, 'FROM', user.host)
+
+
+#Memory
+#psutil.virtual_memory()
+#psutil.swap_memory()
+def mem():
+    print('----MEMORY----')
+    mem_name = ['total', 'available', 'used', 'free', 'active', 'inactive', 'buffers', 'cached', 'shared', 'slab', 'wired']
+    for name, value in zip(mem_name, psutil.virtual_memory()):
+        print('virtual', name, value)
+    swap_name = ['total', 'used', 'free', 'percent']
+    for name, value in zip(swap_name, psutil.swap_memory()):
+         print('swap', name, value)
+mem()
+
+
